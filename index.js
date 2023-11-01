bbb9d219b8ad2aa59bf6a5fec5759a9e4c72bab1
moses <moses.kingi@moringaschool.com>
2023-11-01 05:38:49 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
+ 
+
+
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+      this.radius = radius;
+    }
+  
+ ///correctly returning  the radius property of a Circle instance using `circle.radius`"
+    get diameter() {
+      return this.radius * 2;
+    }
+   //// radius using the pseudo-property `circle.diameter
+   
+    set diameter(diameter) {
+      this.radius = diameter / 2;
+    }
+  
+    get circumference() {
+      return Math.PI * this.diameter;
+    }
+  
+
+    set circumference(circumference) {
+      this.diameter = circumference / Math.PI;
+    }
+  
+ 
+    get area() {
+      return Math.PI * Math.pow(this.radius, 2);
+    }
+  
+    set area(area) {
+      this.radius = Math.sqrt(area / Math.PI);
+    }
+  }
+  
+ 
+  const circle1 = new Circle(5)
+  console.log(circle1.diameter); 
+  console.log(circle1.circumference);    
+  console.log(circle1.area);             
+  
+  circle1.diameter = 14;
+  console.log(circle1.radius);           
+  console.log(circle1.circumference);    
+  console.log(circle1.area);         
+  
+  circle1.circumference = 100;
+  console.log(circle1.radius);           
+  console.log(circle1.diameter);         
+  console.log(circle1.area);             
+  
+  circle1.area = 50;
+  console.log(circle1.radius);           
+  console.log(circle1.diameter);        
+  console.log(circle1.circumference);   
